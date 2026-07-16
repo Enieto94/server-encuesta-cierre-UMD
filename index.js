4eaa5e56e31911040c4e01a7f9904b85ddfd733c
Edwin Nieto <36973704+Enieto94@users.noreply.github.com>
2026-07-15 23:53:21 -0500

    Url productiva front

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3001;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['https://encuesta-cierre-umd-front.onrender.com/'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
