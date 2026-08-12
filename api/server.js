027caa2a879040a6092d33df7b50acf2133d0740
=Edwin Nieto <=egnieto94@gmail.com>
2026-08-12 17:41:43 -0500

    cors for formulario

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3001;
 
 app.use(cors({
-    origin: ['https://encuesta-cierre-umd-front.onrender.com/', 'https://encuesta-cierre-umd-front.onrender.com'],
+    origin: ['https://encuesta-cierre-umd-front.onrender.com/', 'https://encuesta-cierre-umd-front.onrender.com', 'https://formulario-i2ci.onrender.com/api/auth/register', 'https://formulario-i2ci.onrender.com/api/auth/login'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
 }));
